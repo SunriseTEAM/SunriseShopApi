8eee56ff16ecd816fc2dcda6f40587c329041f07
anhtuan01673233538@gmail.com <anhtuan01673233538@gmail.com>
2020-12-04 10:27:04 +0700

    Scroll Top

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {fadeAnimation} from './animations/fadeIntRoute';
 
 @Component({
@@ -9,4 +9,36 @@ import {fadeAnimation} from './animations/fadeIntRoute';
 })
 export class AppComponent {
   title = 'shop';
+  showScroll: boolean;
+  showScrollHeight = 300;
+  hideScrollHeight = 10;
+
+  constructor() { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll()
+  {
+    if (( window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > this.showScrollHeight)
+    {
+      this.showScroll = true;
+    }
+    else if ( this.showScroll && (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) < this.hideScrollHeight)
+    {
+      this.showScroll = false;
+    }
+  }
+
+  ngOnInit(): void {}
+
+  scrollToTop()
+  {
+    (function smoothscroll()
+    { var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0)
+      {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 5));
+      }
+    })();
+  }
 }
